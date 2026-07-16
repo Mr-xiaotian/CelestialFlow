4e313dc7d3b859ca177ea0468f82c87db16412b6
Xiaotian <mingxiaomingtian@gmail.com>
2026-07-16 20:30:44 +0800

    [Chore] delte typings folder

--- typings/dotenv/__init__.pyi
+++
@@ -1,8 +0,0 @@
-def load_dotenv(
-    dotenv_path: str | None = ...,
-    stream: str | None = ...,
-    verbose: bool = ...,
-    interpolate: bool = ...,
-    override: bool = ...,
-    encoding: str | None = ...,
-) -> bool: ...
